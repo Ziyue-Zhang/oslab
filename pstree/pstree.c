954e798ff781bf3fac31ba7d4661ec6d9a65c7cf
tracer-nju <tracer@nju.edu.cn>
2019-03-03 15:22:04 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 520eba1d34e44397b27bf0fe56e3720e     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:22:04 up 13:56,  1 user,  load average: 0.26, 0.13, 0.04

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
 
 struct Node	{
 	int pid;
@@ -7,13 +13,35 @@ struct Node	{
 	int ppid;
 	int son;
 };
+
+void test(char *dir) {
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+	char * name = dir;
+	if((dp = opendir(dir)) == NULL) {
+		printf("Cannot open dir: %s\n", dir);
+		return;
+	}
+	chdir(dir);
+	while ((entry = readdir(dp)) != NULL) {
+		lstat(entry->d_name, &statbuf);
+		if(S_ISDIR(statbuf.st_mode)) {
+			if(entry->d_name[0] >= '0' && entry->d_name[0] <= '9') {
+				printf("%s\n", entry->d_name);
+				test(entry->d_name); 
+			}
+		}
+		else {
+			if(strcmp("ststus", entry->d_name) == 0) {
+				printf("ddhf\n");
+			}
+		}
+	}
+	chdir("..");
+	closedir(dp);
+}
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
-  int i;
-  for (i = 0; i < argc; i++) {
-    assert(argv[i]); // always true
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]); // always true
+  test("/proc:");
   return 0;
 }
