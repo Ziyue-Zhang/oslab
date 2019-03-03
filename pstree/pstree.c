3eb2684b5d269fb85129833b51241208e4652726
tracer-nju <tracer@nju.edu.cn>
2019-03-03 17:49:17 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 520eba1d34e44397b27bf0fe56e3720e     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:49:17 up 15:22,  1 user,  load average: 0.25, 0.13, 0.04

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -10,7 +10,6 @@
 struct Node	{
 	char pid[80];
 	char name[80];
-	int son;
 }node[N];
 int size;
 int cunt;
@@ -60,61 +59,10 @@ void test(char *dir) {
 	chdir("..");
 	closedir(dp);
 }
-void match(char *dir) {
-	DIR *dp;
-	struct dirent *entry;
-	struct stat statbuf;
-	char * name = dir;
-	if((dp = opendir(dir)) == NULL) {
-		printf("Cannot open dir: %s\n", dir);
-		return;
-	}
-	chdir(dir);
-	while ((entry = readdir(dp)) != NULL) {
-		lstat(entry->d_name, &statbuf);
-		if(S_ISDIR(statbuf.st_mode)) {
-			if(entry->d_name[0] >= '0' && entry->d_name[0] <= '9') {
-				printf("%s\n", entry->d_name);
-				match(entry->d_name); 
-			}
-		}
-		else {
-			if(strcmp("status", entry->d_name) == 0) {
-				char filename[80] = "/proc/";
-				strcat(filename, name);
-				strcat(filename, "/status");
-				printf("%s\n", filename);
-				FILE *fp = fopen(filename, "r");
-				if(fp) {
-					char temp[80];
-					while(strcmp(temp, "PPid:") != 0)
-						fscanf(fp, "%s", temp);
-					fscanf(fp, "%s", temp);
-					printf("%s\n", temp);
-				//if(strcmp(node[cunt].pid, temp)!=0)
-				//		printf("wrong\n");
-				    int i = 0;
-					for(; i < size; i++)
-						if(strcmp(node[i].pid, temp) == 0)
-							break;
-					if(i < size) {
-						printf("%d\n", i);
-						node[i].son = cunt;
-					}
-					else
-						printf("wrong\n");
-					cunt++;
-				}
-			}
-		}
-	}
-	chdir("..");
-	closedir(dp);
-}
+
 int main(int argc, char *argv[]) {
   test("/proc");
-  match("/proc");
- // for(int i = 0; i < size; i++)
-	//  printf("%s %s %d\n", node[i].pid, node[i].name, node[i].son);
+  // for(int i = 0; i < size; i++)
+	//  printf("%s %s %d\n", node[i].pid, node[i].name);
   return 0;
 }
