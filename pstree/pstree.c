fe2e6810c1d9bf8219b5da0c872fdd94d99c19c3
tracer-nju <tracer@nju.edu.cn>
2019-03-03 16:05:16 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 520eba1d34e44397b27bf0fe56e3720e     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:05:16 up 14:39,  1 user,  load average: 0.04, 0.06, 0.02

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -45,6 +45,48 @@ void test(char *dir) {
 					while(strcmp(temp, "Name:") != 0)
 						fscanf(fp, "%s", temp);
 					fscanf(fp, "%s", temp);
+					strcpy(node[size].name, temp);
+					while(strcmp(temp, "Pid:") != 0)
+						fscanf(fp, "%s", temp);
+					fscanf(fp, "%s", temp);
+					strcmp(node[size++].pid, temp);
+				}
+			}
+		}
+	}
+	chdir("..");
+	closedir(dp);
+}
+void match(char *dir) {
+	DIR *dp;
+	struct dirent *entry;
+	struct stat statbuf;
+	char * name = dir;
+	if((dp = opendir(dir)) == NULL) {
+		printf("Cannot open dir: %s\n", dir);
+		return;
+	}
+	chdir(dir);
+	while ((entry = readdir(dp)) != NULL) {
+		lstat(entry->d_name, &statbuf);
+		if(S_ISDIR(statbuf.st_mode)) {
+			if(entry->d_name[0] >= '0' && entry->d_name[0] <= '9') {
+				printf("%s\n", entry->d_name);
+				test(entry->d_name); 
+			}
+		}
+		else {
+			if(strcmp("status", entry->d_name) == 0) {
+				char filename[80] = "/proc/";
+				strcat(filename, name);
+				strcat(filename, "/status");
+				printf("%s\n", filename);
+				FILE *fp = fopen(filename, "r");
+				if(fp) {
+					char temp[80];
+					while(strcmp(temp, "Name:") != 0)
+						fscanf(fp, "%s", temp);
+					fscanf(fp, "%s", temp);
 					strcpy(node[size++].name, temp);
 				}
 			}
@@ -56,6 +98,6 @@ void test(char *dir) {
 int main(int argc, char *argv[]) {
   test("/proc");
   for(int i = 0; i < size; i++)
-	  printf("%s\n", node[i].name);
+	  printf("%s %s\n", node[i].pid, node[i].name);
   return 0;
 }
