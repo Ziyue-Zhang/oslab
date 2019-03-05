f95e00c704d44e089fbe1538e0dd0aeeb804e447
tracer-nju <tracer@nju.edu.cn>
2019-03-05 17:34:30 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 757eb3852d674572be4d7e2229dec7f4     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:34:30 up  1:52,  1 user,  load average: 0.10, 0.15, 0.13

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -110,7 +110,7 @@ void printtree(int fa, int len, bool flag) {
 		printf("\n");
 }
 int main(int argc, char *argv[]) {
-  test("/proc");
+  test("/proc","/proc");
   //for(int i = 0; i < size; i++)
 	//printf("%s %s %s\n", node[i].pid, node[i].name, node[i].ppid);
   printtree(root, 0, 0);
