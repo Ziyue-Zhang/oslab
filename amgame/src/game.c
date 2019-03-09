15ad7f54f716c9f025a70c68696c842a80cc7227
tracer-nju <tracer@nju.edu.cn>
2019-03-09 17:34:00 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3f7d23c25660449db3f4c46fe43ca97c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:34:00 up  2:31,  1 user,  load average: 0.59, 0.27, 0.14

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -58,8 +58,8 @@ void splash() {
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
-        draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
-		//draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, color); // white
+        //draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+		draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, color); // white
       }
     }
   }
