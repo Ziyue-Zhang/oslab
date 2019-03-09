6a553bc6030c14671d36c36d22eb881fcf716ed9
tracer-nju <tracer@nju.edu.cn>
2019-03-09 19:28:32 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3f7d23c25660449db3f4c46fe43ca97c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:28:32 up  4:08,  1 user,  load average: 0.11, 0.05, 0.01

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -5,14 +5,16 @@ void init_screen();
 void splash();
 void read_keyy();
 void clean();
-int color;
+void display();
+int color, mw, mh, x, y, px, py;
 void draw_rectt(int x, int y, int w, int h, uint32_t color) ;
 int main() {
   // Operating system is a C program
   _ioe_init();
   init_screen();
   color=0xffffff;
-  splash();
+  //splash();
+  display();
   while (1) {
     read_keyy();
      //splash();
@@ -29,19 +31,39 @@ void read_keyy() {
   };
   _io_read(_DEV_INPUT, _DEVREG_INPUT_KBD, &event, sizeof(event));
   if (event.keycode != _KEY_NONE && event.keydown) {
-    if(strcmp(key_names[event.keycode], "1") == 0) {
+    /*if(strcmp(key_names[event.keycode], "1") == 0) {
         color=(color+0xf)%(0xffffff+1);
         splash();
         }
-    if(strcmp(key_names[event.keycode], "2") == 0){
+    else if(strcmp(key_names[event.keycode], "2") == 0){
         color=(color-0xff00)%(0xffffff+1);
         splash();
         }
-    if(strcmp(key_names[event.keycode], "3") == 0) {
+    else if(strcmp(key_names[event.keycode], "3") == 0) {
         color=(color+0xff00)%(0xffffff+1);
         splash();
+    }*/
+    if(strcmp(key_names[event.keycode], "UP") == 0) {
+        if(py > 0)
+        py--;
+        display();
     }
-    if(strcmp(key_names[event.keycode], "SPACE") == 0) {
+    else if(strcmp(key_names[event.keycode], "LEFT") == 0) {
+        if(px > 0)
+        px--;
+        display();
+    }
+    else if(strcmp(key_names[event.keycode], "RIGHT") == 0) {
+        if(px < mw - 1)
+        px++;
+        display();
+    }
+    else if(strcmp(key_names[event.keycode], "DOWN") == 0) {
+        if(py < mh - 1)
+        py++;
+        display();
+    }
+    else if(strcmp(key_names[event.keycode], "SPACE") == 0) {
         clean();
     }   
     puts("Key pressed: ");
@@ -50,7 +72,7 @@ void read_keyy() {
   }
 }
 
-int w, h, mw, mh, x, y;
+int w, h;
 
 void init_screen() {
   _DEV_VIDEO_INFO_t info = {0};
@@ -59,8 +81,8 @@ void init_screen() {
   h = info.height;
   mw = w / SIDE;
   mh = h / SIDE;
-  x = 0;
-  y = 0;
+  px = 0;
+  py = 0;
 }
 
 void draw_rectt(int x, int y, int w, int h, uint32_t color) {
@@ -95,12 +117,12 @@ void clean() {
     }
   }
 }
-void show() {
+void display() {
     for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
-      if ((x & 1) ^ (y & 1)) {
+      if (x == px && y == py) {
         //draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
-		draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0x000000);
+		draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffffff);
       }
     }
   }
