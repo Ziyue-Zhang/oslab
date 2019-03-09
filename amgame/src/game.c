7447618b1c677866d5654d82efa7c874f0c02954
tracer-nju <tracer@nju.edu.cn>
2019-03-09 15:59:33 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3f7d23c25660449db3f4c46fe43ca97c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:59:34 up 56 min,  1 user,  load average: 0.26, 0.12, 0.07

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -1,8 +1,9 @@
 #include <game.h>
+#include <klib.h>
 
 void init_screen();
 void splash();
-void read_key();
+void read_keyy();
 
 int main() {
   // Operating system is a C program
@@ -10,12 +11,13 @@ int main() {
   init_screen();
   splash();
   while (1) {
-    read_key();
+    read_keyy();
+    printf("xjm\n");
   }
   return 0;
 }
 
-void read_key() {
+void read_keyy() {
   _DEV_INPUT_KBD_t event = { .keycode = _KEY_NONE };
   #define KEYNAME(key) \
     [_KEY_##key] = #key,
@@ -39,7 +41,7 @@ void init_screen() {
   h = info.height;
 }
 
-void draw_rect(int x, int y, int w, int h, uint32_t color) {
+void draw_rectt(int x, int y, int w, int h, uint32_t color) {
   uint32_t pixels[w * h]; // WARNING: allocated on stack
   _DEV_VIDEO_FBCTL_t event = {
     .x = x, .y = y, .w = w, .h = h, .sync = 1,
@@ -56,7 +58,7 @@ void splash() {
     for (int y = 0; y * SIDE <= h; y++) {
       if ((x & 1) ^ (y & 1)) {
         //draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
-		draw_rect(x * SIDE, y * SIDE, SIDE, SIDE, 0xffff00); // white
+		draw_rectt(x * SIDE, y * SIDE, SIDE, SIDE, 0xffff00); // white
       }
     }
   }
