90c7468250c2c21d214d173c4c3d7b715ff6338c
tracer-nju <tracer@nju.edu.cn>
2019-03-26 00:15:26 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3b2133d60b00463e9aa709f09767e3b3     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:15:26 up  9:22,  1 user,  load average: 0.02, 0.02, 0.06

--- libco/co.c
+++ libco/co.c
@@ -2,20 +2,26 @@
 #include <setjmp.h>
 #include "co.h"
 #define MAX_CO 100
-typedef unsigned char   uint8_t;
+typedef unsigned char uint8_t;
 struct co {
-    char *name;
+    bool st;
     int num;
     jmp_buf buf;
     uint8_t stack[4096];
     void *__stack_backup;
 };
 struct co coroutines[MAX_CO];
+struct co *current;
+func_t cu_func;
+func_t cu_arg;
+int cunt;
 
 void co_init() {
+    cunt = 0;
 }
 
 struct co* co_start(const char *name, func_t func, void *arg) {
+  coroutines[cunt].
   func(arg); // Test #2 hangs
   return NULL;
 }
