0e2f7aeb5bdb834b3668a5db76898549e78c163d
tracer-nju <tracer@nju.edu.cn>
2019-03-26 14:46:19 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3b2133d60b00463e9aa709f09767e3b3     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:46:19 up 15:41,  1 user,  load average: 0.18, 0.07, 0.01

--- libco/co.c
+++ libco/co.c
@@ -41,7 +41,7 @@ struct co* co_start(const char *name, func_t func, void *arg) {
   coroutines[cunt].st = true;
   cu_func = func;
   cu_arg = arg;
-  int val = setjmp(coroutines[0].buf);
+  int val = setjmp(coroutines[0].buf);      //if we don't use it, will never return to this function.
   if(!val) {
     __stack = coroutines[cunt].stack + sizeof(coroutines[cunt].stack);
     asm volatile("mov " SP ", %0; mov %1, " SP :
@@ -58,6 +58,9 @@ struct co* co_start(const char *name, func_t func, void *arg) {
     current->st = 0;
     __stack_backup = coroutines[cunt].stack_backup;
     asm volatile("mov %0," SP : : "g"(__stack_backup));
+    
+    current = &coroutines[0];
+    longjmp(current->buf, 1)
     
   }
   return &coroutines[cunt - 1];
