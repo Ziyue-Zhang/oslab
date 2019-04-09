c08ef8534e4624c57a4f3568861c3fa2adb2e8f9
tracer-nju <tracer@nju.edu.cn>
2019-04-09 21:27:10 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: e362bddb18fc4a7fa7486dffdd03a99c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:27:10 up 23:51,  1 user,  load average: 0.01, 0.03, 0.07

---
+++ kernel/include/alloc.h
@@ -0,0 +1,72 @@
+#define NALLOC 1024
+typedef long Align;
+union header {
+	struct {
+		union header *ptr;
+		unsigned size;
+	}s;
+	Align x;
+};
+typedef union header Header;
+static Header base;
+static Header *freep = NULL;
+void *malloc(unsigned nbytes) {
+	Header *p, *prevp;
+	Header *morecore(unsigned);
+	unsigned nunite;
+	nunits = (nbytes+sizeof(Header)-1)/sizeof(header)+1;
+    if((prevp=freep)==NULL) {
+		base.s.ptr = freeptr = prevptr = &base;
+		base.s.size = 0;
+	}
+	for (p = prevp->s.ptr;;prevp = p, p=p->s.ptr) {
+		if(p->s.size>=nunits) {
+			if(p->s.size==nunits)
+				prevp->s.ptr=p->s.ptr;
+			else {
+				p->s.size-=nunits;
+				p+=p->s.size;
+				p->s.size=nunits;
+			}
+			freep=prevp;
+			return (void*) (p+1);
+		}
+		if(p==freep)
+			if((p=morecore(nunits))==NULL)
+				return NULL;
+	}
+}
+static Header *morecore(unsigned nu) {
+	char *cp, *sbrk(int);
+	Header *ip;
+	if(nu < NALLOC)
+		nu = NALLOC;
+	cp = sbrk(nu * sizeof(Header));
+	if(cp == (char *) -1)
+		return NULL;
+	up = (Header *) cp;
+	up->s.size=nu;
+	free((void *)(up+1));
+	return freep;
+}
+void free(void *ap)
+{
+	Header *bp, *p;
+	bp = (Header *)ap - 1;
+	for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+		if(p >= p->s/ptr && (bp > p || bp < p->s.ptr))
+			break;
+	if(bp +bp->size == p->s.ptr) {
+		bp->s.size += p->s.ptr->s.size;
+		bp->s.ptr = p->s.ptr->s.ptr;
+	}
+	else
+		bp->s.ptr = p->s.ptr;
+	if(p + =->size == bp) {
+		p->s.size += bp->s.size;
+		p->s.ptr = bp->s.ptr;
+	}
+	else
+		p->s.ptr = bp;
+	freep = p;
+}
