c481cceeaadb9caceec8e49fe4123c801a05564d
tracer-nju <tracer@nju.edu.cn>
2019-04-09 23:02:12 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: e362bddb18fc4a7fa7486dffdd03a99c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:02:13 up 1 day,  1:26,  1 user,  load average: 0.15, 0.16, 0.08

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,6 +1,20 @@
 #include <common.h>
 #include <klib.h>
-
+#include <am.h>
+void cli() {
+	asm volatile ("cli");
+}
+void sti() {
+	asm volatile ("sti");
+}
+void lock(intptr_t *lk) {
+	cli();
+	while (_atomic_xchg(lk, 1));
+}
+void unlock(intptr_t *lk) {
+	_atomic_xchg(lk, 0);
+	sti();
+}
 extern void *kalloc();
 extern void kfree();
 int temp=0;
