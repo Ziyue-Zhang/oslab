778c7f90c78ec423ab4015bb16749e69e8feba30
tracer-nju <tracer@nju.edu.cn>
2019-04-14 14:40:49 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:40:49 up  3:03,  1 user,  load average: 0.27, 0.28, 0.22

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -36,7 +36,7 @@ static void os_run() {
   _intr_write(1);
   while (1) {
     //lock(&sb);
-    temp++;
+    //temp++;
     //printf("%d\n",temp);
     /*void * a = pmm->alloc(temp);
     pmm->free(a);
