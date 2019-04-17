3088c482a858fca47415a305381a8673c2fee349
tracer-nju <tracer@nju.edu.cn>
2019-04-17 10:13:05 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:13:05 up  9:53,  1 user,  load average: 0.46, 0.24, 0.14

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("%d\n", argc);
+  int pipe(int fildes[2]);
   for(int i = 1; i < argc; ++i){
 	  printf("%s\n", argv[i]);
+  } 
+  if(pipe(fildes)!=0){
+    printf("error!\n");
+    return -1;
+  }
+  pid = fork();
+  if(pid==0){
+    printf("This is son\n");
+  }
+  else{
+    printf("this is father\n");
   }
   return 0;
 }
