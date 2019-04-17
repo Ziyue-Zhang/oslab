a2332a1211455a325a8a763003e16764e239ab4f
tracer-nju <tracer@nju.edu.cn>
2019-04-17 10:13:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:13:42 up  9:53,  1 user,  load average: 0.48, 0.27, 0.15

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+int fildes[2];
 int main(int argc, char *argv[]) {
-  int pipe(int fildes[2]);
+  int pipe(fildes);
   for(int i = 1; i < argc; ++i){
 	  printf("%s\n", argv[i]);
   } 
