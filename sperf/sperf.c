b5e1dd2564414ea0ef22b6df1bffcc47b4d9a0d3
tracer-nju <tracer@nju.edu.cn>
2019-04-28 20:40:12 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:40:12 up 14:23,  1 user,  load average: 0.44, 0.26, 0.15

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -17,18 +17,20 @@ int main(int argc, char *argv[]) {
   }
   int pid = fork();
   if(pid==0){
-     close(fildes[0]);
+    close(fildes[0]);
     printf("This is son\n");
+    FILE * fd = open("/dev/null", O_WRONLY);
+    dup2(fileno(fd), STDOUT_FILENO);
+    fclose(fd);
     dup2(fildes[1],STDERR_FILENO);
-    int fd = open("/dev/null", O_WRONLY);
-    dup2(fd, STDOUT_FILENO);
+    close(fildes[1]);
     execve("/usr/bin/strace", myarg, NULL);
     printf("can you see me?\n");
   }
   else{
     close(fildes[1]);
     printf("this is father\n");
-     close(fildes[0]);
+    close(fildes[0]);
     dup2(fildes[0], STDIN_FILENO);
     while(fgets(mem, 100000, stdin)){
       printf("%s", mem);
