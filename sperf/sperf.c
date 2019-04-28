d9041f0163acf3027fd950e695ccd58614fda952
tracer-nju <tracer@nju.edu.cn>
2019-04-28 15:43:59 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:43:59 up 12:01,  1 user,  load average: 0.27, 0.14, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -13,18 +13,18 @@ int main(int argc, char *argv[]) {
   int pid = fork();
   if(pid==0){
     printf("This is son\n");
-    dup2(fildes[1],2);
-    execve("/usr/bin/strace", myarg,NULL);
+    //dup2(fildes[1],2);
+    execve("/usr/bin/strace", myarg, NULL);
     printf("can you see me?\n");
   }
   else{
     printf("this is father\n");
-    char str[1000000];
+    /*char str[1000000];
     while(1){
-      int num = read(fildes[0],str,sizeof(str));
+      int num = read(fildes[0],str,1000000);
       for(int i = 0; i < num; i++)
         printf("%c", str[i]);
-    }
+    }*/
   }
   return 0;
 }
