feea5ad4bcd5ccf51e55a3997783fc2fe646010c
tracer-nju <tracer@nju.edu.cn>
2019-04-28 21:11:27 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 8d11299d4c5b473aa2fcf4be9fa8d553     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:11:27 up 14:54,  1 user,  load average: 0.25, 0.10, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
     dup2(fildes[0], STDIN_FILENO);
     while(fgets(mem, 1000, stdin)){
       printf("aaa\n");
-      printf("%c", mem[0]);
+      printf("%c", mem[1]);
     }
   }
   return 0;
