8595435dd0b1f0eba8b9fc7d0a3729d08e618886
tracer-nju <tracer@nju.edu.cn>
2019-05-03 21:47:10 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: fda8cb63e1cf457ea543f3c0aeeed7f7     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:47:10 up  8:41,  1 user,  load average: 0.17, 0.21, 0.14

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -7,6 +7,7 @@
 int cnt;
 char str[N];
 char expr[N];
+char name[N];
 int main(int argc, char *argv[]) {
   cnt=0;
   FILE *fp=fopen("/tmp/a.c", "w");  //clear it
@@ -21,8 +22,10 @@ int main(int argc, char *argv[]) {
    }
    else {
      expr[0]='\0';  //clear
+     name[0]='\0';
      str[strlen(str)-1]='\0';
      sprintf(expr,"int __expr_wrap_%d() { return (%s);}\n", cnt, str);
+     sprintf(name,"__expr_wrap_%d",cnt);
      cnt++;
      FILE *fp=fopen("/tmp/a.c", "a");
      if(!fp)
@@ -30,6 +33,11 @@ int main(int argc, char *argv[]) {
      fprintf(fp,"%s", expr);
      fclose(fp);
      system("gcc /tmp/a.c -shared -fPIC -o /tmp/a.so -ldl");
+     void *so = dlopen("/tmp/a.so", RTLD_LAZY);
+     int (*func)()=dlsym(so,name);
+     int value=func();
+     printf(">> %s = %d.\n", str, value);
+     dlclose(so);
    }
   }
   return 0;
