32756769da0522a8875d51f1c5e964a9a2a66c3f
tracer-nju <tracer@nju.edu.cn>
2019-05-03 20:13:05 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: fda8cb63e1cf457ea543f3c0aeeed7f7     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:13:05 up  7:07,  1 user,  load average: 0.23, 0.19, 0.12

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -1,8 +1,31 @@
 #include <dlfcn.h>
 #include <stdio.h>
+#define N 10000
+int cnt;
+char str[N];
+char expr[N];
 int main(int argc, char *argv[]) {
-  FILE *fp=fopen("/tmp/a.c", "w");
-  fprintf(fp,"nmsl\n");
+  cnt=0;
+  FILE *fp=fopen("/tmp/a.c", "w");  //clear it
   fclose(fp);
+  while(fgets(str, N, stdin)){
+   if(str[0]=='i'&&str[1]=='n'&&str[2]=='t'){
+     FILE *fp=fopen("/tmp/a.c", "a");
+     if(!fp)
+       assert(0);
+     fprintf(fp,"%s", str);
+     fclose(fp);
+   }
+   else {
+     expr[0]='\0';  //clear
+     sprintf(expr,"int __expr_wrap_%d() { return (%s);}", cnt, str);
+     cnt++;
+     FILE *fp=fopen("/tmp/a.c", "a");
+     if(!fp)
+       assert(0);
+     fprintf(fp,"%s", expr);
+     fclose(fp);
+   }
+  }
   return 0;
 }
