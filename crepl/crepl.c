c93977bb98a76b7442f53172de1664566afc3e06
tracer-nju <tracer@nju.edu.cn>
2019-05-05 10:58:08 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: fda8cb63e1cf457ea543f3c0aeeed7f7     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  10:58:08 up 10:33,  1 user,  load average: 0.51, 0.34, 0.26

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -47,8 +47,8 @@ int main(int argc, char *argv[]) {
    else {
      bool flag=false;
      for(int i=0;i<strlen(str);i++){
-      if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')) {
-       if((str[i+1]>='a'&&str[i+1]<='z')||(str[i+1]>='A'&&str[i+1]<='Z')||str[i+1]=='('){
+      if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')||str[i]=='_') {
+       if((str[i+1]>='a'&&str[i+1]<='z')||(str[i+1]>='A'&&str[i+1]<='Z')||str[i+1]=='('||str[i+1]=='_'){
        }
        else{
          flag=true;
