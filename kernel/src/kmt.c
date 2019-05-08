ba592ea695ab0bd28eb25aeaf667301519bf9c1d
tracer-nju <tracer@nju.edu.cn>
2019-05-09 00:21:57 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3e39f5266a3349cc99d8f38ecffd19ca     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:21:57 up  5:19,  1 user,  load average: 0.37, 0.21, 0.13

---
+++ kernel/src/kmt.c
@@ -0,0 +1,27 @@
+#include <common.h>
+#include <klib.h>
+typedef struct task task_t;
+typedef struct spinlock spinlock_t;
+typedef struct semaphore sem_t;
+
+ static void init();
+ static int create(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
+ static void teardown(task_t *task);
+ static void spin_init(spinlock_t *lk, const char *name);
+ static void spin_lock(spinlock_t *lk);
+ static void spin_unlock(spinlock_t *lk);
+ static void sem_init(sem_t *sem, const char *name, int value);
+ static void sem_wait(sem_t *sem);
+ static void sem_signal(sem_t *sem);
+
+typedef struct {
+  void (*init)();
+  int (*create)(task_t *task, const char *name, void (*entry)(void *arg), void *arg);
+  void (*teardown)(task_t *task);
+  void (*spin_init)(spinlock_t *lk, const char *name);
+  void (*spin_lock)(spinlock_t *lk);
+  void (*spin_unlock)(spinlock_t *lk);
+  void (*sem_init)(sem_t *sem, const char *name, int value);
+  void (*sem_wait)(sem_t *sem);
+  void (*sem_signal)(sem_t *sem);
+} MODULE(kmt);
