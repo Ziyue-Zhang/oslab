0d05b5296516408d79d5c4b124aa08a30db66c3b
tracer-nju <tracer@nju.edu.cn>
2019-05-09 00:54:52 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3e39f5266a3349cc99d8f38ecffd19ca     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:54:53 up  5:52,  1 user,  load average: 0.10, 0.14, 0.10

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -33,13 +33,13 @@ typedef struct semaphore sem_t;
  }
 
 MODULE_DEF(kmt){
-  .init = kmt_init;
-  .create = kmt_create;
-  .teardown = kmt_teardown;
-  .spin_init = kmt_spin_init;
-  .spin_lock = kmt_spin_lock;
-  .spin_unlock = kmt_spin_unlock;
-  .sem_init = kmt_sem_init;
-  .sem_wait = kmt_sem_wait;
-  .sem_signal = kmt_sem_signal;
+  .init = kmt_init,
+  .create = kmt_create,
+  .teardown = kmt_teardown,
+  .spin_init = kmt_spin_init,
+  .spin_lock = kmt_spin_lock,
+  .spin_unlock = kmt_spin_unlock,
+  .sem_init = kmt_sem_init,
+  .sem_wait = kmt_sem_wait,
+  .sem_signal = kmt_sem_signal,
 };
