ab3e279d7199330addf2e55c36619ebf42b5c8c9
tracer-nju <tracer@nju.edu.cn>
2019-05-09 09:47:37 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3e39f5266a3349cc99d8f38ecffd19ca     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  09:47:37 up  6:26,  1 user,  load average: 0.07, 0.13, 0.14

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -14,6 +14,34 @@ typedef struct semaphore sem_t;
  static void kmt_sem_wait(sem_t *sem);
  static void kmt_sem_signal(sem_t *sem);
 
+ static void kmt_init(){
+	 
+ }
+ static int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
+	 return 0;
+ }
+ static void kmt_teardown(task_t *task){
+
+ }
+ static void kmt_spin_init(spinlock_t *lk, const char *name){
+
+ }
+ static void kmt_spin_lock(spinlock_t *lk){
+
+ }
+ static void kmt_spin_unlock(spinlock_t *lk){
+
+ }
+ static void kmt_sem_init(sem_t *sem, const char *name, int value){
+
+ }
+ static void kmt_sem_wait(sem_t *sem){
+
+ }
+ static void kmt_sem_signal(sem_t *sem){
+
+ }
+
 MODULE_DEF(kmt){
   .init = kmt_init,
   .create = kmt_create,
