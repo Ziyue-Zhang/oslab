bf2c438b0a0bbf5d94d5a5d10a7717a3a1b0ff80
tracer-nju <tracer@nju.edu.cn>
2019-05-15 09:52:24 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 3e4fda255f634f2982e62ab44368502c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  09:52:24 up 10:04,  1 user,  load average: 0.22, 0.26, 0.25

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -68,10 +68,20 @@ _Context *kmt_context_switch (_Event ev, _Context *context){
    lk->cpu = -1;    //this lock isn't on cpu 0,so we can't use 0
  }
  static void kmt_spin_lock(spinlock_t *lk){
-
+  pushcli(); 
+  if(holding(lk))
+    panic("acquire");
+  while(xchg(&lk->locked, 1) != 0);
+  __sync_synchronize();
+  lk->cpu = _cpu();
  }
  static void kmt_spin_unlock(spinlock_t *lk){
-
+  if(!holding(lk))
+    panic("release");
+  lk->cpu = -1;
+  __sync_synchronize();
+  asm volatile("movl $0, %0" : "+m" (lk->locked) : );
+  popcli();
  }
  static void kmt_sem_init(sem_t *sem, const char *name, int value){
 
--- kernel/src/os.c
+++ kernel/src/os.c
@@ -34,16 +34,16 @@ static void hello() {
   unlock(&sb1);
 }
 
-void echo_task(void *name){
+/*void echo_task(void *name){
   device_t *tty = dev_lookup(name);
   while(1){
     char line[128], text[128];
     sprintf(text, "(%s) $ ", name); tty_write(tty, text);
-    int nread = tty->ops->read(tty, 0 ,line, size(line));
+    int nread = tty->ops->read(tty, 0 ,line, sizeof(line));
     line[nread - 1] = '\0';
     sprintf(text, "Echo: %s.\n", line); tty_write(tty, text);
   }
-}
+}*/
 
 static void os_run() {
   hello();
