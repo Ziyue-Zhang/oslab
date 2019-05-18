24dcb90e88d1fb94c565b0c45ba75aa69d97a2ce
tracer-nju <tracer@nju.edu.cn>
2019-05-19 00:13:20 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 69126941e127484693331b1a7bfa54dc     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  00:13:20 up 17:38,  1 user,  load average: 0.30, 0.14, 0.06

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -62,7 +62,7 @@ _Context *kmt_context_save (_Event ev, _Context *context){
   kmt_spin_lock(&LK);
   //printf("nmsl\n");
   if (current) {
-    assert(current->state==RUN);
+    //assert(current->state==RUN);
     current->context = *context;
   }
   kmt_spin_unlock(&LK);
