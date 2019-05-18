8463ccc30993a5fc1dfd1afb461f72918a00ef91
tracer-nju <tracer@nju.edu.cn>
2019-05-18 17:40:07 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 69126941e127484693331b1a7bfa54dc     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:40:07 up 14:15,  1 user,  load average: 0.37, 0.39, 0.34

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -33,7 +33,7 @@ static void producer(void *arg){
     kmt->sem_wait(&empty);
     kmt->sem_wait(&mutex);
     cunt++;
-    printf("%d ", cunt);
+    //printf("%d ", cunt);
     kmt->sem_signal(&mutex);
     kmt->sem_signal(&full);
   }
@@ -44,7 +44,7 @@ static void consumer(void *arg){
     kmt->sem_wait(&full);
     kmt->sem_wait(&mutex);
     cunt--;
-    //printf("%d ", cunt);
+    printf("%d ", cunt);
     kmt->sem_signal(&mutex);
     kmt->sem_signal(&empty);
   }
