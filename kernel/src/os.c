0bc6fbb5f4e4e0f4dde6d59794cb7a195bdd437d
tracer-nju <tracer@nju.edu.cn>
2019-05-19 11:08:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: c0b768823d1e43e7843660df41fa3151     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  11:08:42 up  1:17,  1 user,  load average: 0.13, 0.16, 0.09

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -139,7 +139,7 @@ static void os_init() {
   kmt->create(pmm->alloc(sizeof(task_t)),
               "idle4", idle, (void *)4); 
   _vme_init(pmm->alloc, pmm->free);
-  dev->init();
+  //dev->init();
   /*kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty3");
