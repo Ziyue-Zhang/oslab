818698902bca903f6d063f450cb3d67188c17c6b
tracer-nju <tracer@nju.edu.cn>
2019-06-02 20:44:03 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 46c0cd77d9aa4282bd57221b57814c7c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:44:03 up  9:17,  1 user,  load average: 0.79, 0.25, 0.09

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,13 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+char *copy;
 int main(int argc, char *argv[]) {
+  int fd = open("filesystem/fs.img", O_RDONLY);
+  copy=(char *)mmap(NULL, 2<<29, PROT_READ, MAP_SHARED, fd, 0); //512mb
+  
   return 0;
 }
