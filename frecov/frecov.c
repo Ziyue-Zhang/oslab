2456f2e44f6011e620785fe492f53dabfbb2ed76
tracer-nju <tracer@nju.edu.cn>
2019-06-02 21:23:44 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 46c0cd77d9aa4282bd57221b57814c7c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:23:44 up  9:56,  1 user,  load average: 0.10, 0.04, 0.02

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]) {
   copy=(char *)mmap(NULL, 2<<29, PROT_READ, MAP_SHARED, fd, 0); //512mb
   short sector_bit=*(short *)&copy[0xb];
   int sector_cluster_num=(int)copy[0xd];
+  
+  printf("%d\n",sector_bit);
   printf("%d\n",sector_cluster_num);
   return 0;
 }
