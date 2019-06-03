a8af07c9b504fff18274c1cc7229a797397dbd34
tracer-nju <tracer@nju.edu.cn>
2019-06-03 19:33:22 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 46c0cd77d9aa4282bd57221b57814c7c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:33:22 up 13:40,  1 user,  load average: 0.11, 0.05, 0.07

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -4,26 +4,36 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <stdio.h>
+
 char *copy;
+char shortname[16];
+
 int main(int argc, char *argv[]) {
   int fd = open("filesystem/fs.img", O_RDONLY);
   if(fd==-1)
     printf("nmsl\n");
+  
   copy=(char *)mmap(NULL, 2<<29, PROT_READ, MAP_SHARED, fd, 0); //512mb
   short sector_bit=*(short *)&copy[0xb];
   int cluster_sector_num=(int)copy[0xd];
   int sector_num = *(int *)&copy[0x20];
-  int bit_num = sector_bit * sector_num;
+  
+  int end = sector_bit * sector_num;
 
   int cluster_start=*(int *)&copy[0x2c];
-  //int fat_start = *(short *)&copy[0xe];
-  //int fat_num=*(int *)&copy[0x10];
-  //int fat_sector_num=*(int *)&copy[0x24];
+  int fat_start = *(short *)&copy[0xe];
+  int fat_num=*(int *)&copy[0x10];
+  int fat_sector_num=*(int *)&copy[0x24];
+
+  int start=(fat_start+fat_num*fat_sector_num)*sector_bit;
 
   printf("%d\n",sector_bit);
   printf("%d\n",cluster_sector_num);
   printf("%d\n",sector_num);
-  printf("%d\n", bit_num);
+  printf("%d\n", end);
   printf("%d\n", cluster_start);
+  printf("%d\n", start);
+
+ 
   return 0;
 }
