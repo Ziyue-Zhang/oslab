ba50fcbb149c337d8edb25aebeac961798149d16
tracer-nju <tracer@nju.edu.cn>
2019-06-04 17:55:21 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 46c0cd77d9aa4282bd57221b57814c7c     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  17:55:21 up 17:36,  1 user,  load average: 0.26, 0.12, 0.10

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
   int fat_num=*(int *)&copy[0x10];
   int fat_sector_num=*(int *)&copy[0x24];
 
-  int start=(fat_start+fat_num*fat_sector_num)*sector_bit;
+  int start=(fat_start+fat_num*fat_sector_num+(cluster_start-2)*)*sector_bit;
 
   printf("%d\n",sector_bit);
   printf("%d\n",cluster_sector_num);
@@ -35,8 +35,14 @@ int main(int argc, char *argv[]) {
   printf("%d\n", start);
   int cunt=0;
   for(int i=start;i<end;i++){
-    if(copy[i]=='B'&&copy[i+1]=='M'&&copy[i+2]=='P')
+    if(copy[i]=='B'&&copy[i+1]=='M'&&copy[i+2]=='P'){
+      int base=i-8;
+      int high_c=*(short *)&copy[0x14];
+      int short_c=*(short *)&copy[0x1a]; 
+      int file_cluster= high_c<<16+short_c;
+      printf("%08x\n",file_cluster);
       cunt++;
+    }
   }
   printf("%d\n", cunt);
   return 0;
