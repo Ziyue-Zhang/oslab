f85d40f8c87c759c80ff2431e1315857822daf5f
tracer-nju <tracer@nju.edu.cn>
2019-06-27 12:45:31 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 349d40fc942e48eb8839b32016e30a71     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  12:45:31 up  1:30,  1 user,  load average: 0.00, 0.03, 0.19

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -39,6 +39,9 @@ void atm_lock(intptr_t *lk){
   while(_atomic_xchg(lk,1))
     for(int volatile i=0;i<100;i++);
 }
+void atm_unlock(intptr *lk){
+  _atomic_xchg(lk,0);
+}
 void pushcli(void){
   int eflags = get_efl();
   cli();
