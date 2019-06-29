dc05873cdb3a123dfad73ced62b9b1726021dc55
tracer-nju <tracer@nju.edu.cn>
2019-06-29 16:27:06 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: f54e49d372f84dd5ba51d060367f1d33     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:27:06 up  8:00,  1 user,  load average: 0.26, 0.18, 0.12

--- kernel/framework/kernel.h
+++ kernel/framework/kernel.h
@@ -51,4 +51,19 @@ typedef struct {
   void (*init)();
 } MODULE(dev);
 
+typedef struct {
+  void (*init)();
+  int (*access)(const char *path, int mode);
+  int (*mount)(const char *path, filesystem_t *fs);
+  int (*unmount)(const char *path);
+  int (*mkdir)(const char *path);
+  int (*rmdir)(const char *path);
+  int (*link)(const char *oldpath, const char *newpath);
+  int (*unlink)(const char *path);
+  int (*open)(const char *path, int flags);
+  ssize_t (*read)(int fd, void *buf, size_t nbyte);
+  ssize_t (*write)(int fd, void *buf, size_t nbyte);
+  off_t (*lseek)(int fd, off_t offset, int whence);
+  int (*close)(int fd);
+} MODULE (vfs);
 #endif
