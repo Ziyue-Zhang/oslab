665d40e3eab6f663cb235120e2d0aa260343c17c
tracer-nju <tracer@nju.edu.cn>
2019-06-30 15:44:48 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: f54e49d372f84dd5ba51d060367f1d33     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:44:48 up 22:56,  1 user,  load average: 0.15, 0.18, 0.18

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -22,18 +22,24 @@ int ext2_init(filesystem_t* fs, const char* name, device_t *dev){
     ext2fs->dev=dev;
 }
 
-int ext_read(filesystem_t *fs, int inode, int lne, char *buf){
+int ext_read(filesystem_t *fs, int inode, int len, char *buf){
+    ext2_t* ext2fs=(ext2_t*)fs->myfs;
+    if(ext2fs->block_used[inode]==0){
+        *buf='\0';
+    }
+    int start=inode*8*4096;
+    ext2fs->dev->ops->read(ext2fs->dev,start,buf,len*ext2fs->block_size);
+    return 1;
+}
+
+int ext_write(filesystem_t *fs, int inode, int len, char *buf){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
     return 1;
 }
 
-int ext_read(filesystem_t *fs, int inode, int lne, char *buf){
+int ext_delete(filesystem_t *fs, int inode, int len, char *buf){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
-    return 1;
-}
-
-int ext_delete(filesystem_t *fs, int inode, int lne, char *buf){
-    ext2_t* ext2fs=(ext2_t*)fs->myfs;
+    ext2fs->block_used[inode]=0;
     return 1;
 }
 
