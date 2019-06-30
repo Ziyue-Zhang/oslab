9556c112598dab0a39e90429c40c74b0d2463465
tracer-nju <tracer@nju.edu.cn>
2019-06-30 16:35:30 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  16:35:30 up 1 min,  1 user,  load average: 2.17, 0.91, 0.34

--- kernel/src/vfs.c
+++ kernel/src/vfs.c
@@ -23,10 +23,10 @@
       return 0;
   }
   int vfs_unlink(const char *path){
-      return 0
+      return 0;
   }
   int vfs_open(const char *path, int flags){
-      return 0
+      return 0;
   }
   ssize_t vfs_read(int fd, void *buf, size_t nbyte){
       return 0;
