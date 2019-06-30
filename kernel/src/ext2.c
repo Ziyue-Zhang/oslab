9ecb890efa41055e9e31000deb9e7cb819421bb3
tracer-nju <tracer@nju.edu.cn>
2019-06-30 15:31:05 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: f54e49d372f84dd5ba51d060367f1d33     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  15:31:05 up 22:42,  1 user,  load average: 0.05, 0.07, 0.13

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -1,19 +1,39 @@
-/*#include <common.h>
+#include <common.h>
 #include <klib.h>
 #include <devices.h>
 
 struct ext2{
   uint32_t size;
-  uint32_t inode_num;
   uint32_t block_num;
-  char inode_used[1024];
+  uint32_t block_size;
   char block_used[1024];
+  char block_len[128];
   device_t *dev;
+  char fsname[80];
 }ext2_t;
 
 int ext2_init(filesystem_t* fs, const char* name, device_t *dev){
     ezt2_t* ext2fs=(ezt2_t*)fs->myfs;
     memset(ext2fs,0,sizeof(ext2_t));
-    size=dev->ptr
-}*/
+    ext2fs->size=4096*1024;
+    ext2fs->block_num=1024;
+    ext2fs->block_size=4096;
+    strcpy(ext2fs->fsname,name);
+    ext2fs->dev=dev;
+}
+
+int ext_read(filesystem_t *fs, int inode, int lne, char *buf){
+    ezt2_t* ext2fs=(ezt2_t*)fs->myfs;
+
+}
+
+int ext_read(filesystem_t *fs, int inode, int lne, char *buf){
+    ezt2_t* ext2fs=(ezt2_t*)fs->myfs;
+    
+}
+
+int ext_delete(filesystem_t *fs, int inode, int lne, char *buf){
+    ezt2_t* ext2fs=(ezt2_t*)fs->myfs;
+    
+}
 
