f02faa60c43afa936f99a6e571997a94d21f39d4
tracer-nju <tracer@nju.edu.cn>
2019-07-21 14:26:50 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:26:50 up 17:58,  1 user,  load average: 0.54, 0.30, 0.21

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -15,6 +15,7 @@ typedef struct ext2{
 int ext2_init(filesystem_t* fs, const char* name, device_t *dev){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
     memset(ext2fs,0,sizeof(ext2_t));
+    memset(block_used,0,sizeof(block_used));
     ext2fs->size=4096*1024;
     ext2fs->block_num=1024;
     ext2fs->block_size=4096;
@@ -23,31 +24,38 @@ int ext2_init(filesystem_t* fs, const char* name, device_t *dev){
     return 1;
 }
 
-int ext2_read(filesystem_t *fs, int inode, int len, char *buf){
+int ext2_read(filesystem_t *fs, int inode, uint32_t offset, int len, char *buf){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
     if(ext2fs->block_used[inode]==0){
         *buf='\0';
+        return 1;
     }
-    int start=inode*4*4096;
-    ext2fs->dev->ops->read(ext2fs->dev,start,buf,4*ext2fs->block_size);
+    int start=inode*4096+offset;
+    if(len+offset>ext2fs->block_size);
+        len=ext2fs->block_size-offset;
+    ext2fs->dev->ops->read(ext2fs->dev,start,buf,len);
     return 1;
 }
 
-int ext2_write(filesystem_t *fs, int inode, int len, char *buf){
+int ext2_write(filesystem_t *fs, int inode, uint32_t offset, int len, char *buf){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
-    for(int idx=inode*4,i=0;i<len;i++,idx++){
+    /*for(int idx=inode*4,i=0;i<len;i++,idx++){
         ext2fs->block_used[idx]=1;
-    }
-    int start=inode*4*4096;
-    ext2fs->dev->ops->write(ext2fs->dev,start,buf,4*ext2fs->block_size);
+    }*/
+    ext2fs->block_used[inode]=1;
+    int start=inode*4096+offset;
+    if(len+offset>ext2fs->block_size);
+        len=ext2fs->block_size-offset;
+    ext2fs->dev->ops->write(ext2fs->dev,start,buf,len);
     return 1;
 }
 
-int ext2_delete(filesystem_t *fs, int inode, int len, char *buf){
+int ext2_delete(filesystem_t *fs, int inode){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
-    for(int idx=inode*4,i=0;i<len;i++,idx++){
-        ext2fs->block_used[idx]=0;
-    }
+    ext2fs->block_used[inode]=0;
+    char buf[4096]={'\0'};
+    int start=inode*4096;
+    ext2fs->dev->ops->write(ext2fs->dev,start,buf,block_size-offset);
     return 1;
 }
 
