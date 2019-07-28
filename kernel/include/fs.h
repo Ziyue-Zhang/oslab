7df1b5c37edc2cee0fad31e8eb7f5fc7c6dfd8c0
tracer-nju <tracer@nju.edu.cn>
2019-07-28 14:42:20 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  14:42:20 up 1 day,  8:04,  1 user,  load average: 0.30, 0.26, 0.19

--- kernel/include/fs.h
+++ kernel/include/fs.h
@@ -1,17 +1,1 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
 
-#include <devices.h>
-#include <common.h>
-#include <klib.h>
-
-typedef struct ext2{
-	  uint32_t size;
-	    uint32_t block_num;
-		  uint32_t block_size;
-		    char block_used[1024];
-			  device_t *dev;
-			    char fsname[80];
-}ext2_t;
-
-#endif
