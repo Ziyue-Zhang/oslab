b3bd94a67e491e0e5c9e3e2f85eeb5a56d9b1879
tracer-nju <tracer@nju.edu.cn>
2019-07-28 23:14:39 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  23:14:40 up 1 day, 16:36,  1 user,  load average: 0.25, 0.18, 0.18

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -11,6 +11,14 @@ typedef struct ext2{
 	char fsname[80];
 }ext2_t;
 
+int ext2_init(filesystem_t* fs, const char* name, device_t *dev);
+int ext2_alloc(filesystem_t *fs, int inode);
+int ext2_free(filesystem_t *fs, int inode);
+int ext2_lookup(filesystem_t *fs, const char *path, int flags);
+int ext2_read(filesystem_t *fs, int inode, uint32_t offset, int len, char *buf);
+int ext2_write(filesystem_t *fs, int inode, uint32_t offset, int len, char *buf);
+int ext2_delete(filesystem_t *fs, int inode);
+
 int ext2_init(filesystem_t* fs, const char* name, device_t *dev){
     ext2_t* ext2fs=(ext2_t*)fs->myfs;
     memset(ext2fs,0,sizeof(ext2_t));
