5a7c5157095486dc067c4a86f6109efb71ee6606
tracer-nju <tracer@nju.edu.cn>
2019-07-29 22:22:06 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:22:06 up 1 day, 17:00,  1 user,  load average: 0.20, 0.27, 0.27

--- kernel/include/vfs.h
+++ kernel/include/vfs.h
@@ -5,6 +5,51 @@
 #include <common.h>
 #include <klib.h>
 
+typedef struct Vinode{
+  char path[200];
+  char name[80];
+  int dotdot;
+  int dot;
+  int nxt;
+  int son;
+  int type;
+  int link_inode;
+  int link_count;
+  int filesystem;       // vfs read/write/lseek must know it
+  filesystem_t *fs;  
+}vinode_t;
+typedef struct fileroot{
+  char name[80];
+  filesystem_t *fs;
+  int son;
+  int use; 
+}fileroot_t;
+fileroot_t mount_table[16];
+vinode_t vinode[1024];
 
+void vinode_free(int idx);
+int vinode_alloc(int type);
+int vinode_setroot();
+int vinode_lookup(char *path);
+int vinode_dot(int id);
+int vinode_dotdot();
+int vinode_dir();
+int vinode_file();
+
+
+void vfs_init();
+int vfs_lookup(char *path);
+int vfs_access(const char *path, int mode);
+int vfs_mount(const char *path, filesystem_t *fs);
+int vfs_unmount(const char *path);
+int vfs_mkdir(const char *path);
+int vfs_rmdir(const char *path);
+int vfs_link(const char *oldpath, const char *newpath);
+int vfs_unlink(const char *path);
+int vfs_open(const char *path, int flags);
+ssize_t vfs_read(int fd, void *buf, size_t nbyte);
+ssize_t vfs_write(int fd, void *buf, size_t nbyte);
+off_t vfs_lseek(int fd, off_t offset, int whence);
+int vfs_close(int fd);
 
 #endif
