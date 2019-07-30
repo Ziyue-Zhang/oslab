5cd9297bd3d9afc7dd3b2728f9378d47af8b2743
tracer-nju <tracer@nju.edu.cn>
2019-07-30 20:38:42 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:38:42 up 2 days,  3:58,  1 user,  load average: 0.41, 0.24, 0.20

--- kernel/src/proc.c
+++ kernel/src/proc.c
@@ -61,27 +61,29 @@ int proc_dir(filesystem_t* fs, vinode_t *node, int num){
 ssize_t proc_read(int id, uint64_t offset, char *buf){
     if(!offset)
         return 0;
+    int k=0;
     if(id==2){
-        ret+=sprintf(buf+ret,"\ncpuinfo:\n");
+        k+=sprintf(buf+k,"\ncpuinfo:\n");
         for(int i=0;i<_ncpu();i++){
             int j=jobs[i].inode;
-            ret+=sprintf(buf+ret,"pid:%d\n",j-4);
-            ret+=sprintf(buf+ret,"name:%d\n",procfs[j].name);
-            ret+=sprintf(buf+ret,"cpu_number:%d\n",procfs[j].cpu);
+            k+=sprintf(buf+k,"pid:%d\n",j-4);
+            k+=sprintf(buf+k,"name:%d\n",procfs[j].name);
+            k+=sprintf(buf+k,"cpu_number:%d\n",procfs[j].cpu);
         }
     }
     else if(id==3){
-        ret+=sprintf(buf+ret,"\nmeminfo:\n");
-        ret+=sprintf(buf+ret,"using mem: %d kb\n",use_mem/1024);
-        ret+=sprintf(buf+ret,"free mem: %d kb\n",(tot_mem-use_mem)/1024);
-        ret+=sprintf(buf+ret,"tot mem: %d kb\n",tot_mem/1024);
+        k+=sprintf(buf+k,"\nmeminfo:\n");
+        k+=sprintf(buf+k,"using mem: %d kb\n",use_mem/1024);
+        k+=sprintf(buf+k,"free mem: %d kb\n",(tot_mem-use_mem)/1024);
+        k+=sprintf(buf+k,"tot mem: %d kb\n",tot_mem/1024);
     }
     else{
-        ret+=sprintf(buf+ret,"\ntaskinfo:\n");
-        ret+=sprintf(buf+ret,"pid:%d\n",j-4);
-        ret+=sprintf(buf+ret,"name:%d\n",procfs[j].name);
-        ret+=sprintf(buf+ret,"cpu_number:%d\n",procfs[j].cpu);
-        ret+=sprintf(buf+ret,"mem:%d\n",procfs[j].mem);
-        ret+=sprintf(buf+ret,"cpu_time:%d\n",procfs[j].time);
+        k+=sprintf(buf+k,"\ntaskinfo:\n");
+        k+=sprintf(buf+k,"pid:%d\n",j-4);
+        k+=sprintf(buf+k,"name:%d\n",procfs[j].name);
+        k+=sprintf(buf+k,"cpu_number:%d\n",procfs[j].cpu);
+        k+=sprintf(buf+k,"mem:%d\n",procfs[j].mem);
+        k+=sprintf(buf+k,"cpu_time:%d\n",procfs[j].time);
     }
+    return k;
 }
