28125180456feb628f47f96c2ac3c43b24d0fdc7
tracer-nju <tracer@nju.edu.cn>
2019-07-30 20:02:14 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  20:02:14 up 2 days,  3:22,  1 user,  load average: 0.16, 0.08, 0.09

--- kernel/include/proc.h
+++ kernel/include/proc.h
@@ -11,7 +11,7 @@ typedef struct proc{
     char name[20];
     int inode;
     int cpu;
-    int times;
+    int time;
     int mem;
 }proc_t;
 
--- kernel/src/proc.c
+++ kernel/src/proc.c
@@ -3,7 +3,8 @@
 int jobs[4];
 proc_t procfs[PROC_SIZE];
 int proc_num=0;
-
+uint64_t use_mem=0;
+uint64_t tot_mem=0;
 void *proc_add(char *name){
     if(proc_num==PROC_SIZE){
         return NULL;
@@ -12,7 +13,17 @@ void *proc_add(char *name){
     proc_num++;
     procfs[id].cpu=-1;
     procfs[id].mem=0;
+    procfs[id].time=0
     procfs[id].inode=id-4;
     strcpy(procfs[id].name,name);
     return &procfs[id];
 }
+void proc_mem(uint64_t mem_size, int flag){
+    if(flag==0){
+        use_mem+=mem_size;
+    }
+    else if(flag==1){
+        use_mem-=mem_size;
+    }
+}
+void
