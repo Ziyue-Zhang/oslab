19cbc361eec9cdfe28a2dde036f49c2d67b98308
tracer-nju <tracer@nju.edu.cn>
2019-07-30 19:56:44 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  19:56:44 up 2 days,  3:16,  1 user,  load average: 0.16, 0.08, 0.10

--- kernel/src/proc.c
+++ kernel/src/proc.c
@@ -2,3 +2,17 @@
 
 int jobs[4];
 proc_t procfs[PROC_SIZE];
+int proc_num=0;
+
+void *proc_add(char *name){
+    if(proc_num==PROC_SIZR){
+        return NULL;
+    }
+    int id=proc_num;
+    proc_num++;
+    procfs[id].cpu=-1;
+    procfs[id].mem=0;
+    procfs[id].inode=id-4;
+    strcpy(procfs[id].name,name);
+    return &procfs[id];
+}
