a2a63c9423eb19df22f4f20248e25bfc4a1c551a
tracer-nju <tracer@nju.edu.cn>
2019-07-31 21:57:25 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:57:25 up 2 days, 18:40,  1 user,  load average: 0.38, 0.27, 0.17

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -52,6 +52,7 @@ int ext2_write(filesystem_t *fs, int inode, uint64_t offset, int len, char *buf)
     int start=inode*4096+offset;
     if(len+offset>ext2fs->block_size)
         len=ext2fs->block_size-offset;
+    printf("nms\n");
     ext2fs->dev->ops->write(ext2fs->dev,start,buf,len);
     return len;
 }
