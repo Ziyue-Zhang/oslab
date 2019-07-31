64d9925714331f31f32a5d572fb0d8ce9f61e60c
tracer-nju <tracer@nju.edu.cn>
2019-07-31 22:43:53 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  22:43:54 up 2 days, 19:27,  1 user,  load average: 0.56, 0.68, 0.64

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -293,10 +293,11 @@ void command_cat(char *line, char *text){
     char buf[80];
     de->ops->read(de,start,buf,vinode[id].size);
     printf("%s",buf);*/
-    char temp[80];
+    char temp[200];
     ext2_read(&mount_table[r0],id,0,vinode[id].size,temp);
+    temp[vinode[id].size]='\0';
     //printf("%s\n",temp);
-    //printf("%s\n",temp);
+    printf("%s\n",temp);
     printf("nmsl\n");
     strcat(text,temp);
   }
