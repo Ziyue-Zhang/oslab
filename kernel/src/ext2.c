a7e7bcbbb1af054e3dab8e9920321897da1d76c8
tracer-nju <tracer@nju.edu.cn>
2019-07-31 21:57:04 +0800

    Static hostname: debian          Icon name: computer-vm            Chassis: vm         Machine ID: f21aab52f77e49c183924d7e5c69c7fb            Boot ID: 13b237fd046041ffab663c29ad24707a     Virtualization: vmware   Operating System: Debian GNU/Linux 9 (stretch)             Kernel: Linux 4.9.0-7-amd64       Architecture: x86-64  21:57:04 up 2 days, 18:40,  1 user,  load average: 0.24, 0.24, 0.15

--- kernel/src/ext2.c
+++ kernel/src/ext2.c
@@ -68,8 +68,8 @@ int ext2_delete(filesystem_t *fs, int inode){
 int ext2_build(int fa){
     printf("nmsl\n");
     int id=vinode_addfile(fa, FILE, "a.txt", EXT2, &mount_table[r0]);
-    //char buf[80]={"hello\n"};
-    //ext2_write(&mount_table[r0],id,0,strlen(buf),buf);
+    char buf[80]={"hello\n"};
+    ext2_write(&mount_table[r0],id,0,strlen(buf),buf);
     id=vinode_addfile(fa, FILE, "hello.cpp", EXT2, &mount_table[r0]);
     //char buf2[200]={"#include<iostream>\nusing namespace std;\nint main(){\n    cout<<\"hello world\"<<endl;\n    return 0;\n}\n"};
     //ext2_write(&mount_table[r0],id,0,strlen(buf2),buf2);
